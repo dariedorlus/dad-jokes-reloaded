8e03a580483cad4d66bae186af454ffdaf3eefd1
Tda <tdorluss@gmail.com>
2024-03-04 19:56:46 -0500

    Darie's really  joke

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="flex flex-row justify-between px-4 bg-white border-solid border-b-[1px] top-0 left-0 right-0 fixed">
+      <img src="/dad-jokes-logo.svg" height={60} width={60} />
+      <div className="pt-2">
+        <p className="text-2xl font-bold">Darie's a Joke</p>
+      </div>
+    </div>
+  );
+};
